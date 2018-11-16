103a194a54a4367b6b989521696aebaf0fa8627c
andrejsb <39262126+andrejsb@users.noreply.github.com>
2018-11-16 16:45:13 +0200

    Update QuizQuestions.js

--- src/components/QuizQuestions.js
+++ src/components/QuizQuestions.js
@@ -43,17 +43,17 @@ class QuizQuestions extends Component {
                     </fieldset>
                 </form>
                 <div>
-                    <legend>7. Some question for Baccarat scores
+                    <legend>7. Baccarat is a comparing card game played between two hands, the Player and the Banker. Would you calculate the value of last hand on the picture?
                     </legend>
                     <img width="800px" src={scoreQuiz}/>
                     <div className="form-check"><label><input type="radio" className="form-check-input"
-                                                              name="{questions[i].answers[j].name}"/>14</label></div>
-                    <div className="form-check"><label><input type="radio" className="form-check-input"
-                                                              name="{questions[i].answers[j].name}"/>9</label></div>
+                                                              name="{questions[i].answers[j].name}"/>18</label></div>
                     <div className="form-check"><label><input type="radio" className="form-check-input"
                                                               name="{questions[i].answers[j].name}"/>1</label></div>
                     <div className="form-check"><label><input type="radio" className="form-check-input"
-                                                              name="{questions[i].answers[j].name}"/>4</label></div>
+                                                              name="{questions[i].answers[j].name}"/>8</label></div>
+                    <div className="form-check"><label><input type="radio" className="form-check-input"
+                                                              name="{questions[i].answers[j].name}"/>9</label></div>
                 </div>
             </div>
     )
