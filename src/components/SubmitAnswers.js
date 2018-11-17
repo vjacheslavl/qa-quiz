d8fd8dba46a1b249cd9946dbfad8f019fd22f27c
vjacheslavl <vjacheslav.lukashevich@gmail.com>
2018-11-17 15:03:21 +0200

    add REST POST request

--- src/components/SubmitAnswers.js
+++ src/components/SubmitAnswers.js
@@ -11,7 +11,32 @@ class SubmitAnswers extends Component {
 
     handleClick(e) {
         console.log(this.props.answers);
+        insertSomething(this.props.answers).then((result) => {
+            console.log("Result is" + result)
+
+        });
         this.props.history.push('/')
+    }
+
+}
+
+async function insertSomething(params) {
+    console.log("params are  " + params)
+    try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        });
+        let responseJson = await response.json();
+        console.log("RJSON "+responseJson)
+        return responseJson.result;
+    }
+    catch (error) {
+        console.log("Error is - " + error);
     }
 
 }
