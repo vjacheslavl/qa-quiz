62b5abef9beae5c1b176e711a46e7f1efda42322
vjacheslavl <vjacheslav.lukashevich@gmail.com>
2018-11-20 22:32:00 +0200

    Add logo

--- src/PageRoutes.js
+++ src/PageRoutes.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
 import App from "./App";
 import Home from "./components/Home";
+import logo from "./images/qa_logo.png";
 import CompletedPage from "./components/CompletedPage";
 
 class PageRoutes extends Component {
     render() {
         return (
             <div className="container">
-                <div className="text-center mb-4"><h1>Evo QA Quiz!</h1></div>
+                <div className="text-center mb-4"><img src={logo}/></div>
                 <BrowserRouter>
                     <div>
                         <Route path="/" component={Home} exact/>
