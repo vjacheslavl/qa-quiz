8a3a0a9c451c5f5e32585baf85f9276f70813031
vjacheslavl <vjacheslav.lukashevich@gmail.com>
2018-11-21 22:12:31 +0200

    results calculation

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {withRouter} from "react-router-dom";
+import quizQuestionsList from '../data/quizQuestions';
+import wrongCodeLines from '../data/wrongCodeLines';
 
 class ResultsPage extends Component {
     constructor(props) {
@@ -54,7 +56,8 @@ class ResultsPage extends Component {
             const differences = data[i].differences.filter((item) => item.found === true).length;
             const answers = countCorrectQuestions(data[i]);
             const codeLine = countCodeLines(data[i]);
-            const total = 20;
+            const total = differences + answers + codeLine;
+
             participants.push(<tr key={i}>
                 <td>{data[i].personName}</td>
                 <td>{answers}</td>
@@ -69,11 +72,24 @@ class ResultsPage extends Component {
 
 }
 
-function countCorrectQuestions(datum) {
-    return 44;
+function countCorrectQuestions(item) {
+    return item.answers.filter((answer) => isCorrectAnswer(answer)).length;
 }
-function countCodeLines(datum) {
-    return 33;
+
+function isCorrectAnswer(answerData) {
+    const foundQuestion = quizQuestionsList.filter((questionItem) => questionItem.name === answerData.question)[0];
+    const foundAnswer = foundQuestion.answers.filter((answerItem) => answerItem.id === answerData.answer);
+    return foundAnswer.length === 0 ? false : foundAnswer[0].correct;
+}
+
+function countCodeLines(item) {
+    return item.codeLines.filter((codeline) => isCorrectlyMarkedCodeline(codeline)).length;
+
+}
+
+function isCorrectlyMarkedCodeline(codelineData) {
+    const foundCodeline = wrongCodeLines.filter((codelineItem) => (codelineItem.line - 1) === codelineData.key)[0];
+    return foundCodeline.bad === codelineData.marked;
 }
 
 export default withRouter(ResultsPage);
---
+++ src/data/wrongCodeLines.js
@@ -0,0 +1,117 @@
+let wrongCodeLines = [
+        {
+            line: 1,
+            bad: false
+        },
+        {
+            line: 2,
+            bad: true
+        },
+        {
+            line: 3,
+            bad: false
+        },
+        {
+            line: 4,
+            bad: false
+        },
+        {
+            line: 5,
+            bad: true
+        },
+        {
+            line: 6,
+            bad: false
+        },
+        {
+            line: 7,
+            bad: false
+        },
+        {
+            line: 8,
+            bad: true
+        },
+        {
+            line: 9,
+            bad: false
+        },
+        {
+            line: 10,
+            bad: true
+        },
+        {
+            line: 11,
+            bad: true
+        },
+        {
+            line: 12,
+            bad: false
+        },
+        {
+            line: 13,
+            bad: false
+        },
+        {
+            line: 14,
+            bad: false
+        },
+        {
+            line: 15,
+            bad: false
+        },
+        {
+            line: 16,
+            bad: false
+        },
+        {
+            line: 17,
+            bad: true
+        },
+        {
+            line: 18,
+            bad: false
+        },
+        {
+            line: 19,
+            bad: false
+        },
+        {
+            line: 20,
+            bad: true
+        },
+        {
+            line: 21,
+            bad: true
+        },
+        {
+            line: 22,
+            bad: true
+        },
+        {
+            line: 23,
+            bad: false
+        },
+        {
+            line: 24,
+            bad: true
+        },
+        {
+            line: 25,
+            bad: true
+        },
+        {
+            line: 26,
+            bad: true
+        },
+        {
+            line: 27,
+            bad: true
+        },
+        {
+            line: 28,
+            bad: false
+        }
+    ]
+;
+
+export default wrongCodeLines;
